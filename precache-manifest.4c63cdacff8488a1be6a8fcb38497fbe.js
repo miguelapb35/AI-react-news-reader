54dfe32f36c7343fba60892db2692a790b5ca768
Miguel Angel <miguelapb35@gmail.com>
2020-08-17 20:35:25 -0500

    Updates

---
+++ precache-manifest.4c63cdacff8488a1be6a8fcb38497fbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "039750dad97b89559b47dfd185e3bd9f",
+    "url": "/AI-react-news-reader/index.html"
+  },
+  {
+    "revision": "ebf6af23ee6adcfdac60",
+    "url": "/AI-react-news-reader/static/css/main.3c7b1384.chunk.css"
+  },
+  {
+    "revision": "1b4532ebdf142f098d8e",
+    "url": "/AI-react-news-reader/static/js/2.d02e797c.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/AI-react-news-reader/static/js/2.d02e797c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebf6af23ee6adcfdac60",
+    "url": "/AI-react-news-reader/static/js/main.d59663de.chunk.js"
+  },
+  {
+    "revision": "77552f902c861517080f",
+    "url": "/AI-react-news-reader/static/js/runtime-main.030014af.js"
+  }
+]);
